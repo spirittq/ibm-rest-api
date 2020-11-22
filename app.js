faeba339483ed4d11f53453fbb186258e21f81e3
spirittq <37908532+spirittq@users.noreply.github.com>
2020-11-22 15:57:48 +0200

    Extract needed data from JSON and return new JSON

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const url = 'https://www.vrk.lt/statiniai/puslapiai/rinkimai/rt.json';
 app.get('/api/v1/elections', (req, res) =>{
   tools.getData(url, (error, body) =>{
     if (error) return console.log(error);
-    console.log(body);
+    body = JSON.parse(body);
+    const electionsArray = [];
+
+    body.data.forEach((entry) => {
+      const election = {
+        date: entry.rink_data,
+        name: entry.vrt_pav,
+        id: entry.vrt_id
+      };
+      electionsArray.push(election);
+    });
+    res.send(JSON.stringify(electionsArray, null, 2));
   });
 });
 
